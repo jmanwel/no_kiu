086bdf0c3c1d3978064064a472f8cf1d62b46ea3
NICOLAS ORLANDO <norlando@alumno.unlam.edu.ar>
2021-05-16 21:18:37 -0300

    First commit.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noku.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
